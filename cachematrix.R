102502b30b2a7e883f55d01f2111464f0ad6a6f2
michaelpdunlap <michael.p.dunlap76@gmail.com>
2020-01-11 15:22:03 -0500

    Completed ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this function creates an environment 
+## that will allow for the storage of a matrix
+## and a list of get/set based functions to 
+## set or extract values from it
+## created 1/11/2020
+## last updated 1/11/2020
 
 makeCacheMatrix <- function(x = matrix()) {
+     inv <-NULL
+     set <-function (y) {
+          x <<-y
+          inv <<- NULL
+     }
+     get <- function() x
+     setinverse <- function(mean) inv <<- mean
+     getinverse <- function() inv
+     list (set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of the matrix
+## defined within an environment of type makeCacheMatrix
+## created 1/11/2020
+## last updated 1/11/2020
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     inv <- x$getinverse()
+     if(!is.null(inv)) {
+          message("getting cached data")
+          return (inv)
+     }
+     data <- x$get()
+     inv <- solve (data, ...)
+     x$setinverse(inv)
+     inv
 }
